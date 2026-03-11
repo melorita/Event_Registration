cd5d52d8e6a00b8951c62c00dac32823d4e9c78a
Lidia Shenkut <liduuruha@gmail.com>
2026-03-11 11:02:27 -0700

    some things changed

--- EvenetRegistrationDesktop/Program.cs
+++ EvenetRegistrationDesktop/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EvenetRegistrationDesktop.forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,8 @@ namespace EvenetRegistrationDesktop
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+            Application.Run(new EventManagementForm());
         }
     }
 }
