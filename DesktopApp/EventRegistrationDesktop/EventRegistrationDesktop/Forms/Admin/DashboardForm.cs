bb8e64f84fd3c40589e2406daf284622abd26746
MierafA12 <mierafabebe12@gmail.com>
2026-03-13 10:30:06 +0300

    dashboard form updated

--- DesktopApp/EventRegistrationDesktop/EventRegistrationDesktop/Forms/Admin/DashboardForm.cs
+++ DesktopApp/EventRegistrationDesktop/EventRegistrationDesktop/Forms/Admin/DashboardForm.cs
@@ -8,15 +8,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using EventRegistrationDesktop.Forms.Components;
+
 namespace EventRegistrationDesktop.Forms.Admin
 {
-    public partial class DashboardForm : Form
+    public partial class DashboardForm : BaseDashboardForm
     {
-        private Form activeForm = null;
-        private Button currentButton = null;
-        private Color defaultBtnColor = SystemColors.Control;
-        private Color hoverBtnColor = Color.LightSkyBlue;
-        private Color activeBtnColor = Color.DeepSkyBlue;
 
         public DashboardForm()
         {
@@ -28,66 +25,14 @@ namespace EventRegistrationDesktop.Forms.Admin
             btnparticipants.Click += (s, e) => ActivateButton(btnparticipants);
             btnReports.Click += (s, e) => ActivateButton(btnReports);
 
-            SetupSidebarButtons();
+            SetupSidebarButtons(panelsidebar);
 
-            // Set dashboard active at first
             ActivateButton(btndashboard);
-        }
-
-        private void SetupSidebarButtons()
-        {
-            foreach (Control ctrl in panelsidebar.Controls)
-            {
-                if (ctrl is Button btn && btn.Name != "btnLogout")
-                {
-                    btn.FlatStyle = FlatStyle.Flat;
-                    btn.FlatAppearance.BorderSize = 0;
-                    btn.BackColor = defaultBtnColor;
-
-                    btn.MouseEnter += (s, e) => {
-                        if (currentButton != btn)
-                            btn.BackColor = hoverBtnColor;
-                    };
-
-                    btn.MouseLeave += (s, e) => {
-                        if (currentButton != btn)
-                            btn.BackColor = defaultBtnColor;
-                    };
-                }
-            }
-        }
-
-        private void ActivateButton(Button btn)
-        {
-            if (btn == null) return;
-
-            // Reset previous button
-            if (currentButton != null)
-            {
-                currentButton.BackColor = defaultBtnColor;
-            }
-
-            // Set new active button
-            currentButton = btn;
-            currentButton.BackColor = activeBtnColor;
         }
 
         private void openChildForm(Form childForm)
         {
-            if (activeForm != null)
-                activeForm.Close();
-            
-            activeForm = childForm;
-            childForm.TopLevel = false;
-            childForm.FormBorderStyle = FormBorderStyle.None;
-            childForm.Dock = DockStyle.Fill;
-            
-            panelmain.Controls.Add(childForm);
-            panelmain.Tag = childForm;
-            childForm.BringToFront();
-            childForm.Show();
-            
-            lblTitle.Text = "Event Registration System - " + childForm.Text;
+            base.openChildForm(childForm, panelmain, lblTitle);
         }
 
         private void Btndashboard_Click(object sender, EventArgs e)
@@ -109,7 +54,7 @@ namespace EventRegistrationDesktop.Forms.Admin
 
         private void BtnLogout_Click(object sender, EventArgs e)
         {
-            EventRegistrationDesktop.Forms.LoginForm login = new EventRegistrationDesktop.Forms.LoginForm();
+            EventRegistrationDesktop.Forms.Components.LoginForm login = new EventRegistrationDesktop.Forms.Components.LoginForm();
             login.Show();
             this.Close();
         }
