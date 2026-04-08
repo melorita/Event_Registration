9153d28269c6820118607dc3c2236d580952a9f3
MierafA12 <mierafabebe12@gmail.com>
2026-04-08 09:48:38 +0300

    project DTO and services created

---
+++ Backend/EventRegistration.API/DTOs/AuthDto.cs
@@ -0,0 +1,23 @@
+namespace EventRegistration.API.DTOs
+{
+    public class UserDto
+    {
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string Token { get; set; }
+        public string Role { get; set; }
+    }
+
+    public class LoginDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class RegisterDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+    }
+}
---
+++ Backend/EventRegistration.API/Services/IUserService.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EventRegistration.Core.Entities;
+
+namespace EventRegistration.API.Services
+{
+    public interface IUserService
+    {
+        Task<User> AuthenticateAsync(string email, string password);
+        Task<User> RegisterAsync(User user, string password);
+    }
+}
