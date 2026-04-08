637d60fad8dc28b4bc57c66aec92d3120965bea9
MierafA12 <mierafabebe12@gmail.com>
2026-04-08 12:51:11 +0300

    authdto

--- Backend/EventRegistration.API/DTOs/AuthDto.cs
+++ Backend/EventRegistration.API/DTOs/AuthDto.cs
@@ -19,5 +19,7 @@ namespace EventRegistration.API.DTOs
         public string Email { get; set; }
         public string Password { get; set; }
         public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
     }
 }
