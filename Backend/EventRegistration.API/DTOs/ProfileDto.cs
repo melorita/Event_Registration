cae11b40056d94f4a383877c3f70e29008c2cbdb
MierafA12 <mierafabebe12@gmail.com>
2026-04-09 16:29:09 +0300

    profile dto update

---
+++ Backend/EventRegistration.API/DTOs/ProfileDto.cs
@@ -0,0 +1,10 @@
+namespace EventRegistration.API.DTOs
+{
+    public class ProfileUpdateDto
+    {
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string ProfilePictureUrl { get; set; }
+    }
+}
