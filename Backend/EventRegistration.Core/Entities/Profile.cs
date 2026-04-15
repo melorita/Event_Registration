763153ae3618eebb7196b457829ccd7d0d074688
MierafA12 <mierafabebe12@gmail.com>
2026-04-15 14:08:56 +0300

    profile entitiy update by adding gender

--- Backend/EventRegistration.Core/Entities/Profile.cs
+++ Backend/EventRegistration.Core/Entities/Profile.cs
@@ -9,6 +9,7 @@ namespace EventRegistration.Core.Entities
         public User User { get; set; }
         public string PhoneNumber { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
+        public string Gender { get; set; } = string.Empty;
         public string ProfilePictureUrl { get; set; } = string.Empty;
         public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     }
