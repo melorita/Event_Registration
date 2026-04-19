34335eed46018fd85f5611d5ce4a63b9fed91d39
MierafA12 <mierafabebe12@gmail.com>
2026-04-19 17:28:25 +0300

    registration model fixed

---
+++ DesktopApp/EventRegistrationDesktop/EventRegistrationDesktop/Models/RegistrationDto.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace EventRegistrationDesktop.Models
+{
+    public class RegistrationDto
+    {
+        public int Id { get; set; }
+        public int EventId { get; set; }
+        public string EventName { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Status { get; set; }
+        public DateTime RegisteredAt { get; set; }
+        public string EventDate { get; set; }
+        public string EventLocation { get; set; }
+        public string PaymentReceiptImage { get; set; }
+    }
+}
