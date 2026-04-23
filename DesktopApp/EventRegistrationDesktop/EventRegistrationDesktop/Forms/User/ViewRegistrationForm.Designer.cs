49b7f01a102dba131b24b783dd186fba348345c7
Bethel953 <bethel9531@gmail.com>
2026-04-23 21:13:36 +0300

    view registration page updated

--- DesktopApp/EventRegistrationDesktop/EventRegistrationDesktop/Forms/User/ViewRegistrationForm.Designer.cs
+++ DesktopApp/EventRegistrationDesktop/EventRegistrationDesktop/Forms/User/ViewRegistrationForm.Designer.cs
@@ -3,12 +3,10 @@ namespace EventRegistrationDesktop.Forms.User
     partial class ViewRegistrationForm
     {
         /// <summary>
-        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
